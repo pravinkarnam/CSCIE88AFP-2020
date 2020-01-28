494bf4d398d97b28fd150e439cad8b7e04220466
Dhyan <dhyanesh@netapp.com>
2020-01-27 21:26:31 -0500

    Script to download and install jdk in tools folder

---
+++ fp-java-demo/tools/install-jdk.sh
@@ -0,0 +1,2 @@
+wget https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.6%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.6_10.tar.gz
+tar -xvf OpenJDK11U-jdk_x64_linux_hotspot_11.0.6_10.tar.gz
