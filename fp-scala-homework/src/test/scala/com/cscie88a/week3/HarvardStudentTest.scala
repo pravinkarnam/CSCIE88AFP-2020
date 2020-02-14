17126d496f5b4df416ace2ef5d5d56cbff8e1fe5
esumitra <edward.sumitra@gmail.com>
2020-02-13 20:13:45 -0500

    skeleton code for assignment 3 (#19)
    * Update application.conf
    
    * add zip homework task
    
    * added unit test skeleton
    
    Co-authored-by: pavithrabv <pavi@dhyans.com>

---
+++ fp-scala-homework/src/test/scala/com/cscie88a/week3/HarvardStudentTest.scala
@@ -0,0 +1,21 @@
+package com.cscie88a.week3
+
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+
+class HarvardStudentTest
+    extends WordSpec
+    with Matchers
+    with BeforeAndAfterAll {
+
+
+  "HarvardStudent case class" should {
+    // add unit tests for problem 4 below
+
+  }
+
+  "HarvardStudent list operations" should {
+    // add unit tests for problem 5 below
+
+  }
+
+}
