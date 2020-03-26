d55b744d035b6a51377553dadda4d8e05b0d85a6
Praveen Karnam <pravinkarnam@gmail.com>
2020-03-26 06:58:57 -0700

    comm

--- fp-scala-homework/src/main/scala/com/cscie88a/week6/FunctionUtils2.scala
+++ fp-scala-homework/src/main/scala/com/cscie88a/week6/FunctionUtils2.scala
@@ -1,5 +1,49 @@
 package com.cscie88a.week6
 
 object FunctionUtils2 {
-  // add you code here
+  val prefixLogger: String => (String => String) = prefix => str => prefix + " " + str
+
+  def sumInt(inList: List[Int]): Int = {
+    inList.foldRight(0)(_ + _)
+  }
+
+  def productInt(inList: List[Int]): Int = {
+    inList.foldRight(1)(_ * _)
+  }
+
+
+  def all(inList: List[Boolean]): Boolean = {
+    inList.foldRight(true)(_ && _)
+  }
+
+
+  def any(inList: List[Boolean]): Boolean = {
+    inList.foldRight(false)(_ || _)
+  }
+
+
+  def tokenize(text: List[String]): List[String] = {
+    for {
+      line  <- text
+      word  <- line.split(" ")
+    }
+      yield word
+  }
+
+  def updateCounts(word: String, counts: Map[String, Int]): Map[String, Int] = {
+    val valueOfWord = counts getOrElse(word, 1)
+    if (counts contains word){
+      counts + (word -> (valueOfWord + 1))
+    }
+    else {
+      counts + (word->valueOfWord)
+    }
+  }
+
+//  def wordCount(text: List[String]): Map[String, Int] = {
+//    val words:List[String] = tokenize(text)
+//
+//
+//  }
+
 }
--- fp-scala-homework/src/test/scala/com/cscie88a/week6/FunctionUtils2Test.scala
+++ fp-scala-homework/src/test/scala/com/cscie88a/week6/FunctionUtils2Test.scala
@@ -13,4 +13,50 @@ class FunctionUtils2Test
         true should be (false)
       }
     }
+
+  "prefixLogger" should {
+    "prefix" in {
+      FunctionUtils2.prefixLogger("<scala>")("rocks") should be ("<scala> rocks")
+    }
+  }
+  "sumInt" should {
+    "sum" in {
+      FunctionUtils2.sumInt(List(3, 4, 5)) should be (12)
+    }
+  }
+  "productInt" should {
+    "multiply" in {
+      FunctionUtils2.productInt(List(2, 3, 4)) should be (24)
+    }
+  }
+  "all" should {
+    "return true if all items in list are true" in {
+      FunctionUtils2.all(List(true, true, true)) should be (true)
+    }
+    "return false if at-least one item in list is false" in {
+      FunctionUtils2.all(List(false, true, true)) should be (false)
+    }
+  }
+  "any" should {
+    "return true if atleast one item in list is true" in {
+      FunctionUtils2.any(List(true, false, false)) should be (true)
+    }
+    "return false if all items in list is false" in {
+      FunctionUtils2.any(List(false, false, false)) should be (false)
+    }
+  }
+  "tokenize" should {
+    "return the tokens of words" in {
+      FunctionUtils2.tokenize((List("This is Praveen", "Prasanna is awesome", "Pranav is cute"))) should be
+      (List("This", "is", "Praveen", "Prasanna", "is", "awesome", "Pranav", "is", "cute"))
+    }
+  }
+  "updateCounts" should {
+    "return counts of words in list updating the current word count" in {
+      FunctionUtils2.updateCounts("this", Map("this" -> 5, "is" -> 1)) should be (Map("this" -> 6, "is" -> 1))
+    }
+    "add the word with count 1 if the word does not exists" in {
+      FunctionUtils2.updateCounts("a", Map("this" -> 5, "is" -> 1)) should be (Map("this" -> 5, "is" -> 1, "a" -> 1))
+    }
+  }
 }
